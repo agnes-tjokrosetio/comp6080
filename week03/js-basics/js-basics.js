17789b04b12f9e8e6451c217d6538003ecabacfb
agnes-tjokrosetio <agnes.tjokrosetio@gmail.com>
2026-03-02 23:27:14 +1100

    week three

--- week03/js-basics/js-basics.js
+++ week03/js-basics/js-basics.js
@@ -1,4 +1,15 @@
-// Basic syntax: Variables, Conditionals, Printing
+// Variables
+
+let name = "Alice"; // can change later
+const age = 20; // cannot change
+
+// Printing
+
+console.log("Hello world"); // prints to console
+let score = 95;
+console.log(score);
+
+// Conditionals
 
 // Arrays & Loops
 
